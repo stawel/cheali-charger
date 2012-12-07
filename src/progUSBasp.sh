0cc2dcb24297ce6eef4821a15b5bd4107e4cfe4d
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2012-12-07 21:12:25 +0100

    add usbasp script

--- src/progUSBasp.sh
+++ src/progUSBasp.sh
@@ -7,6 +7,6 @@ TTY=/dev/ttyUSB0
 
 echo "HEX=$HEX"
 echo "TTY=$TTY"
-avrdude -patmega32 -cavrisp -P$TTY -b38400 -Uflash:w:$HEX:a $*
+avrdude -patmega32 -cusbasp -P$TTY -Uflash:w:$HEX:a $*
 
 #-C/home/stawel/aaa/arduino/arduino-1.0/hardware/tools/avrdude.conf 
