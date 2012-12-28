aaea053c141a417b1f904653e0150426463ebd9c
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2012-12-28 16:50:31 +0100

    add StaticMenu.h

---
+++ src/StaticMenu.h
@@ -0,0 +1,23 @@
+#ifndef STATICMENU_H_
+#define STATICMENU_H_
+
+#include "LcdPrint.h"
+#include "memory.h"
+#include "Menu.h"
+
+class StaticMenu : public Menu {
+public:
+	const char * const* staticMenu_;
+
+public:
+	StaticMenu(const char * const* staticMenu,uint8_t size):
+			Menu(size), staticMenu_(staticMenu){}
+
+
+	virtual uint8_t printItem(uint8_t i) {
+		return lcdPrint_P(pgm::read(&staticMenu_[i]));
+	}
+};
+
+#endif /* STATICMENU_H_ */
+
