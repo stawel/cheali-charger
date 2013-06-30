8db6308e7cd149baff91f2634d0dccf04194c4a8
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2013-06-30 13:54:26 +0200

    avrdude: create backup

--- src/hardware/GTPowerA6-10/progUSBasp.sh
+++ src/hardware/GTPowerA6-10/progUSBasp.sh
@@ -4,11 +4,24 @@ set -x
 
 #maybe you should change this:
 TTY=/dev/ttyUSB0
+PROGRAMMER=usbasp
+#---
+
+PARTNO=atmega32
 
 HEX=`ls cheali-charger*.hex`
-
 echo "HEX=$HEX"
 echo "TTY=$TTY"
-avrdude -patmega32 -cusbasp -P$TTY -Uflash:w:$HEX:a $*
 
-#-C/home/stawel/aaa/arduino/arduino-1.0/hardware/tools/avrdude.conf 
+if [ "$1" != "--no-backup" ]; then
+    NOW=`date +"%F_%T"`
+    DIR="backup/$NOW"
+    echo "creating buckup: $DIR"
+    mkdir -p $DIR
+    cd $DIR
+    avrdude -p$PARTNO -c$PROGRAMMER -P$TTY -Uflash:r:flash.hex:i -Ulfuse:r:lfuse.hex:i -Uhfuse:r:hfuse.hex:i -Ueeprom:r:eeprom.hex:i
+    cd -
+fi
+
+
+avrdude -p$PARTNO -c$PROGRAMMER -P$TTY -Uflash:w:$HEX:a
