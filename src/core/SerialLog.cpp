fce375f9daef6c9c073828a9c51f2f67cd5048e2
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2013-10-26 19:24:37 +0200

    copy arduino/HardwareSerial.* to Serial.*

--- src/core/SerialLog.cpp
+++ src/core/SerialLog.cpp
@@ -15,8 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <Arduino.h>
-
+#include "Serial.h"
 #include "SerialLog.h"
 #include "Hardware.h"
 #include "Timer.h"
