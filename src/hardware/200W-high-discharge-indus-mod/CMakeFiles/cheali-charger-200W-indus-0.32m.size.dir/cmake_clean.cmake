b5310893e501a94c6a25aecb48b358fb8f56ca77
njozsef <nagy.jozsef.tv2@gmail.com>
2014-04-10 14:45:29 +0200

    cleaned

--- src/hardware/200W-high-discharge-indus-mod/CMakeFiles/cheali-charger-200W-indus-0.32m.size.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/cheali-charger-200W-indus-0.32m.size"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/cheali-charger-200W-indus-0.32m.size.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- src/hardware/50W-5A/CMakeFiles/cheali-charger-50W-5A-0.32m.size.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/cheali-charger-50W-5A-0.32m.size"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/cheali-charger-50W-5A-0.32m.size.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
