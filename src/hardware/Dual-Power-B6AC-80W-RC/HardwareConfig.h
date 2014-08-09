1744d04980dfc5a89de9e6566c028536a733ae7d
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2014-08-09 02:25:23 +0200

    bugfix: Dual-Power-B6AC-80W-RC max charge current

--- src/hardware/Dual-Power-B6AC-80W-RC/HardwareConfig.h
+++ src/hardware/Dual-Power-B6AC-80W-RC/HardwareConfig.h
@@ -33,7 +33,7 @@
 
 
 //1-13? correlation
-#define SMPS_UPPERBOUND_VALUE               (MAX_CHARGE_I*13)
+#define SMPS_UPPERBOUND_VALUE               65000
 #define DISCHARGER_UPPERBOUND_VALUE         TIMER1_PRECISION_PERIOD
 
 #endif /* HARDWARE_CONFIG_H_ */
