4463193c646389c939406d4559f392ea48c305dc
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2015-01-06 17:11:37 +0100

    arm, soft. uart: changing volatile to std::atomic

--- arm-compiler.cmake
+++ arm-compiler.cmake
@@ -16,7 +16,7 @@ CMAKE_FORCE_CXX_COMPILER(${CMAKE_CXX_COMPILER} GNU)
 SET(CTUNING "-funsigned-char -funsigned-bitfields -fshort-enums")
 
 SET(CFLAGS "-mthumb -mcpu=cortex-m0  ${CTUNING} -Os  -Wall -g -std=c11")
-SET(CXXFLAGS "-mthumb -mcpu=cortex-m0  ${CTUNING} -Os  -Wall -g -fno-rtti -fno-exceptions")
+SET(CXXFLAGS "-mthumb -mcpu=cortex-m0  ${CTUNING} -Os  -Wall -g -fno-rtti -fno-exceptions -std=c++11")
 SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${CFLAGS}   -ffunction-sections -fdata-sections")
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXXFLAGS} -ffunction-sections -fdata-sections")
 
--- src/hardware/nuvoton-M0517/cpu/TxSoftSerial.cpp
+++ src/hardware/nuvoton-M0517/cpu/TxSoftSerial.cpp
@@ -18,7 +18,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "atomic.h"
+#include <atomic>
 #include "Hardware.h"
 #include "TxSoftSerial.h"
 #include "irq_priority.h"
@@ -32,12 +32,12 @@ namespace TxSoftSerial {
 #define START_BIT 0
 #define STOP_BIT 512
 
-uint8_t  pucTxBuffer[Tx_BUFFER_SIZE];
-volatile uint16_t usTxData;
-volatile uint32_t *pucTxpin;
+uint8_t  txBuffer_[Tx_BUFFER_SIZE];
+uint16_t txData_;
+volatile uint32_t *txPin_;
 
-volatile uint16_t usTxBufferRead;
-volatile uint16_t usTxBufferWrite;
+std::atomic<uint16_t> tail_(0);
+std::atomic<uint16_t> head_(0);
 
 
 void initialize()
@@ -46,7 +46,7 @@ void initialize()
     CLK_SetModuleClock(TMR2_MODULE,CLK_CLKSEL1_TMR2_S_HCLK,CLK_CLKDIV_UART(1));
     NVIC_SetPriority(TMR2_IRQn, SOFTWARE_SERIAL_IRQ_PRIORITY);
     NVIC_EnableIRQ(TMR2_IRQn);
-    pucTxpin = IO::getPinAddress(UART_TX_PIN);
+    txPin_ = IO::getPinAddress(UART_TX_PIN);
 
 #ifndef ENABLE_EXT_TEMP_AND_UART_COMMON_OUTPUT
     IO::pinMode(UART_TX_PIN, GPIO_PMD_OUTPUT);
@@ -64,10 +64,8 @@ void begin(unsigned long baud)
     TIMER_Open(TIMER2, TIMER_PERIODIC_MODE, baud);
     TIMER_EnableInt(TIMER2);
 
-    *(pucTxpin) = 1;     // Tx pin high (IDLE)
-    usTxBufferRead = 0;
-    usTxBufferWrite = 0;
-    usTxData = 0;
+    *(txPin_) = 1;     // Tx pin high (IDLE)
+    txData_ = 0;
 
     TIMER_Start(TIMER2);
 }
@@ -75,19 +73,22 @@ void begin(unsigned long baud)
 
 void write(uint8_t ucData)
 {
-    uint16_t usTemp = (usTxBufferWrite + 1) % Tx_BUFFER_SIZE;
+    uint16_t i = (head_.load(std::memory_order_relaxed) + 1) % Tx_BUFFER_SIZE;
 
-    while(usTemp == usTxBufferRead);
+    while(i == tail_.load(std::memory_order_acquire));
 
-    pucTxBuffer[usTxBufferWrite] = ucData;
-    __sync_synchronize();
-    usTxBufferWrite = usTemp;
+    txBuffer_[i] = ucData;
+    head_.store(i,  std::memory_order_release);
 }
 
 
 void flush()
 {
-    while(usTxBufferRead != usTxBufferWrite);
+    bool empty;
+    do {
+        uint16_t i = tail_.load(std::memory_order_acquire);
+        empty = (i == head_.load(std::memory_order_relaxed)) && (txData_ == 0);
+    } while(!empty);
 }
 
 void end()
@@ -97,18 +98,20 @@ void end()
 }
 
 inline void getNewData() {
-    if(usTxBufferRead == usTxBufferWrite) return;
+    uint16_t i = tail_.load(std::memory_order_relaxed);
+    if(i == head_.load(std::memory_order_acquire)) return;
 
-    usTxData = START_BIT + ((pucTxBuffer[usTxBufferRead]) << 1) + STOP_BIT;
-    usTxBufferRead = (usTxBufferRead + 1) % Tx_BUFFER_SIZE;
+    i = (i + 1) % Tx_BUFFER_SIZE;
+    txData_ = START_BIT + ((txBuffer_[i]) << 1) + STOP_BIT;
+    tail_.store(i, std::memory_order_release);
 }
 
 extern "C"
 {
 void TMR2_IRQHandler(void) {
-    if(usTxData) {
-        *(pucTxpin) = usTxData & 1;
-        usTxData >>= 1;
+    if(txData_) {
+        *(txPin_) = txData_ & 1;
+        txData_ >>= 1;
     } else {
         getNewData();
     }
