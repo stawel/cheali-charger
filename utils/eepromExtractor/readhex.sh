b78c281fe3cc6759914870b2a634658847de960d
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2015-02-12 21:31:53 +0100

    rename eepromExtractor2 to eepromExtractor

--- utils/eepromExtractor/readhex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INFILE=$1
-OUTFILE=eeprom_hex.bin
-VALUES=./dump.txt
-
-avr-objcopy -I ihex -O binary $INFILE $OUTFILE
-./eepromExtractor $OUTFILE > $VALUES
-./createDefaultCalibration
