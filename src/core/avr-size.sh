3e061c77fc9d766d40ef3a48e5ddc0534ce7b8ae
Kevin <smeatsan@gmail.com>
2015-07-31 11:24:03 +0200

    Detect if avr-size supports mcu parameter.

--- src/core/avr-size.sh
+++ src/core/avr-size.sh
@@ -4,8 +4,16 @@ INPUT="$1"
 OUTPUT="$2"
 echo "INPUT=$INPUT"
 echo "OUTPUT=$OUTPUT"
+
+SIZE_PARAMETER="--format=avr --mcu=atmega32"
+#test if avr-size is patched to support --format=avr and mcu parameter
+avr-size --format=avr --mcu=atmega32 "$INPUT" > /dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+	SIZE_PARAMETER="--format=sysv"
+fi
+
 avr-gcc -v 2>&1 | tee "$OUTPUT"
-avr-size --format=avr --mcu=atmega32 "$INPUT" | tee -a "$OUTPUT"
+avr-size $SIZE_PARAMETER "$INPUT" | tee -a "$OUTPUT"
 
 true
 
