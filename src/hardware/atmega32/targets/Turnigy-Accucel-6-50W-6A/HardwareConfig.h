20546bffa2a17eadb54bd9e0d76fe131ff0da467
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2016-10-16 18:25:29 +0200

    atmega32: Turnigy-Accucel-6-50W-6A

---
+++ src/hardware/atmega32/targets/Turnigy-Accucel-6-50W-6A/HardwareConfig.h
@@ -0,0 +1,38 @@
+/*
+    cheali-charger - open source firmware for a variety of LiPo chargers
+    Copyright (C) 2016 Paweł Stawicki. All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef HARDWARE_CONFIG_H_
+#define HARDWARE_CONFIG_H_
+
+//based on Štěpán Dalecký settings: https://groups.google.com/d/msg/cheali-charger/fcT8gRyPqow/1riRFk83AQAJ
+
+#include "GlobalConfig.h"
+#include "HardwareConfigGeneric.h"
+
+#define MAX_CHARGE_V            ANALOG_VOLT(27.000)
+#define MAX_CHARGE_I            ANALOG_AMP(6.000)
+#define MAX_CHARGE_P            ANALOG_WATT(50.000)
+
+#define MAX_DISCHARGE_P         ANALOG_WATT(5.000)
+#define MAX_DISCHARGE_I         ANALOG_AMP(1.000)
+
+
+//1-13? correlation
+#define SMPS_UPPERBOUND_VALUE               65000
+#define DISCHARGER_UPPERBOUND_VALUE         TIMER1_PRECISION_PERIOD
+
+#endif /* HARDWARE_CONFIG_H_ */
---
+++ src/hardware/atmega32/targets/Turnigy-Accucel-6-50W-6A/defaultCalibration.cpp
@@ -0,0 +1,54 @@
+/*
+    cheali-charger - open source firmware for a variety of LiPo chargers
+    Copyright (C) 2013  Paweł Stawicki. All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "AnalogInputsPrivate.h"
+#include "memory.h"
+
+//file provided by: Jozsef N.
+//TODO: calibrate Vb6_pin, Textern
+
+const AnalogInputs::DefaultValues AnalogInputs::inputsP_[AnalogInputs::PHYSICAL_INPUTS] PROGMEM = {
+    {{0, 0},                        {26368, 12120}},    //Vout
+    {{0, 0},                        {26613, 12518}},    //VreversePolarity
+    {{712, 50},                        {11311, 1000}},    //Ismps
+    {{2320, 50},                        {12160, 300}},    //Idischarge
+
+    {{0, 0},                        {0, 0}},    //VoutMux
+    {{0, 0},                        {0, 0}},    //Tintern
+    {{0, 0},                        {51758, 15230}},    //Vin
+    {{5884,  2280},         {0,  0}},   //Textern
+
+    {{0, 0},                        {48963, 3752}},    //Vb0_pin
+    {{0, 0},                        {54656, 4198}},    //Vb1_pin
+    {{0, 0},                      {53376, 8068}},  //Vb2_pin
+    {{0, 0},                      {54577, 4193}},  //Vb3_pin
+
+    {{0, 0},                      {54976, 4197}},  //Vb4_pin
+    {{0, 0},                      {54400, 4197}},  //Vb5_pin
+    {{0, 0},                      {54400, 4197}},  //Vb6_pin??
+
+#if MAX_BANANCE_CELLS > 6
+    {{0, 0},                        {50752, ANALOG_VOLT(3.865)}},   //Vb7_pin
+    {{0, 0},                        {51008, ANALOG_VOLT(3.885)}},   //Vb8_pin
+#endif
+    //1-1 correlation
+    {{710, 50},                      {11310, 1000}},  //IsmpsSet
+    {{260, 50},                      {1490, 300}},  //IdischargeSet
+};
+
+
