d8b5badf4427fd2ca87ed204b23f66d83737be87
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2018-01-12 22:31:05 +0100

    move nuvoton files to one directory

--- arm-compiler.cmake
+++ arm-compiler.cmake
@@ -7,4 +7,4 @@ SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${CFLAGS}   -ffunction-sections -fdata-s
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXXFLAGS} -ffunction-sections -fdata-sections")
 
 #TODO: remove CoIDE dependency
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -Wl,-T${CMAKE_SOURCE_DIR}/arm-gcc-link.ld")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -Wl,-T${CMAKE_SOURCE_DIR}/src/hardware/nuvoton-M0517/cpu/CMSIS/arm-gcc-link.ld")
--- src/hardware/nuvoton-M0517/cpu/cpu.cmake
+++ src/hardware/nuvoton-M0517/cpu/cpu.cmake
@@ -4,7 +4,7 @@ set(CURRENT_PATH ${CMAKE_CURRENT_LIST_DIR})
 set(CPU_SOURCE
     atomic.h  cpu.h  irq_priority.h  memory.h    Serial.h    TxHardSerial.cpp  TxSoftSerial.cpp  UtilsD.cpp
     cpu.cpp   IO.h   memory.cpp      Serial.cpp  Timer0.cpp  TxHardSerial.h    TxSoftSerial.h    other.c
-    startup_M051Series.c
+    CMSIS/startup_M051Series.c
     StackInfo.cpp
     IO.cpp
 )
